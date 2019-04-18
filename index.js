25f77c3d22ce495096cf7ca36ef3c9ad09dad817
Caio Tavares <caio.tavares11@gmail.com>
2019-04-17 21:41:28 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const request = require('request-promise-native');
+const cheerio = require('cheerio');
+
+const URI = 'http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos'
+const options = {
+  uri: URI,
+  transform: (body) => {
+    return cheerio.load(body)
+  }
+}
+
+function sort(data) {
+  return data.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+}
+
+function combine(data) {
+  let result = [];
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data[i].name === data[i + 1].name) {
+      result.push({
+        name: data[i].name,
+        date: data[i].date,
+        buyPrice: data[i].buyPrice || data[i + 1].buyPrice,
+        sellPrice: data[i].sellPrice || data[i + 1].sellPrice
+      });
+    }
+  }
+  return result;
+}
+
+function extract($) {
+  let result = [];
+  $('.camposTesouroDireto')
+    .each((i, v) => {
+      let values = $(v).children('.listing')
+      result[i] = {
+        name: $(v).children('.listing0').text(),
+        date: $(values[0]).text(),
+        interest: $(values[1]).text(),
+        buyPrice: values.length == 4 ? $(values[3]).text() : undefined,
+        sellPrice: values.length == 3 ? $(values[2]).text() : undefined
+      }
+    })
+  return result;
+}
+
+request(options)
+  .then($ => extract($))
+  .then(r => sort(r))
+  .then(r => combine(r))
+  .then(r => console.log(r))
